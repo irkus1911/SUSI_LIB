e33c1cae718b73cba19f81bae3f5d8e4841d2e20
TARTANGALH\2dam <penemax572@gmail.com>
2021-10-25 10:38:00 +0200

    Interfaces creadas con comentarios de javadoc

--- src/lib/interfaces/Logicable.java
+++ src/lib/interfaces/Logicable.java
@@ -20,8 +20,41 @@ import lib.exceptions.ConnectException;
  */
 public interface Logicable {
     
+    /**
+     * Metodo signIn para iniciar sesion en nuestra aplicacion
+     * @param user Parametro de tipo User enviada desde fuera
+     * @return Devolvera un objeto de tipo usuario
+     * @throws IncorrectUserException Lanza la excepcion cuando el patron del usuario 
+     * no es el adecuado
+     * @throws IncorrectPasswordException Lanza la excepcion cuando el patron de
+     * la contraseña no es el adecuado
+     * @throws UserDontExistException Lanza la excepcion si el usuario introducido
+     * no existe en la base de datos
+     * @throws ConnectException Lanza la excepcion cuando no se ha podido conectar
+     * con la base de datos
+     */
+    
     public User signIn(User user) throws IncorrectUserException, IncorrectPasswordException, 
             UserDontExistException, ConnectException;
+    
+    
+    /**
+     * Motodo singUp para registrarse en nuestra aplicacion
+     * @param user Parametro tipo User enviada desde fuera
+     * @return Devolvera un objeto de tipo usuario
+     * @throws IncorrectUserException Lanza la excepcion cuando el patron del usuario 
+     * no es el adecuado
+     * @throws IncorrectPasswordException Lanza la excepcion cuando el patron de
+     * la contraseña no es el adecuado
+     * @throws IncorrectEmailException Lanza la excepcion cuando el patron del email
+     * no es el adecuado
+     * @throws UserExistException Lanza la excepcion cuando el usuario introducido
+     * ya esta en la base de datos
+     * @throws PasswordDontMatchException Lanza la excepcion cuando las contraseñas
+     * del campo Password y ConfirmPassword no son iguales
+     * @throws ConnectException Lanza la excepcion cuando no se ha podido conectar
+     * con la base de datos
+     */
     
     public User signUp(User user) throws IncorrectUserException, IncorrectPasswordException, 
             IncorrectEmailException, UserExistException, PasswordDontMatchException,
